bcbbf0e4a6614bd96f4604574aae1c21aabd18e2
unknown <BT_3W2_30@BKTECH.LOCAL>
2022-11-23 11:59:48 -0500

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module pingpong.com.ulmasovunitproject14 {
+    requires javafx.controls;
+    requires javafx.fxml;
+        requires javafx.web;
+            
+        requires org.controlsfx.controls;
+            requires com.dlsc.formsfx;
+            requires validatorfx;
+            requires org.kordamp.ikonli.javafx;
+            requires org.kordamp.bootstrapfx.core;
+            requires eu.hansolo.tilesfx;
+            requires com.almasb.fxgl.all;
+    
+    opens pingpong.com.ulmasovunitproject14 to javafx.fxml;
+    exports pingpong.com.ulmasovunitproject14;
+}
