e1f7cd996f0fc6990d46179d147d029bb26e37c4
BT_3W2_30 <amirjonu@nycstudents.net>
2022-11-23 12:00:20 -0500

    turtles

--- src/main/java/pingpong/com/ulmasovunitproject14/HelloApplication.java
+++ src/main/java/pingpong/com/ulmasovunitproject14/HelloApplication.java
@@ -1,19 +1,18 @@
 package pingpong.com.ulmasovunitproject14;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
+    public void start(Stage stage) {
+        stage.setTitle("Ping Pong");
+        StackPane
         stage.show();
     }
 
